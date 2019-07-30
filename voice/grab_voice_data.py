801ac4c3c16ab98efdb58aa8d9f51fa15a6e5606
Wyatt Ellison <ellisonwt@gmail.com>
2019-07-30 09:22:24 -0400

    voice data files and pull code

---
+++ voice/grab_voice_data.py
@@ -0,0 +1,19 @@
+import pickle
+
+def grab_voice_data():
+    '''
+    grabs voice data from voice_data.p
+
+    parameters:
+    ----------
+    None
+
+    returns:
+    --------
+    [np.array] of shape (10,4,44100*4)
+        for 10 person, 4 recordings each, with 4 seconds @ 44100 Hz sampling rate
+    '''
+    f = open(voice_data.p,'rb')
+    out = pickle.load(f)
+    f.close
+    return out
