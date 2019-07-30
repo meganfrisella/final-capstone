73e38a8ae676df681eaafe565ebe3ea6a9559e8c
Wyatt Ellison <ellisonwt@gmail.com>
2019-07-30 09:24:10 -0400

    fixed bug

--- voice/grab_voice_data.py
+++ voice/grab_voice_data.py
@@ -13,7 +13,7 @@ def grab_voice_data():
     [np.array] of shape (10,4,44100*4)
         for 10 person, 4 recordings each, with 4 seconds @ 44100 Hz sampling rate
     '''
-    f = open(voice_data.p,'rb')
+    f = open('voice_data.p','rb')
     out = pickle.load(f)
     f.close
     return out
