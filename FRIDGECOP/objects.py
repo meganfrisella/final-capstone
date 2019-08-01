60f01c946a26519bdf9213107f6f38860bd09aef
Wyatt Ellison <ellisonwt@gmail.com>
2019-08-01 16:43:15 -0400

    made only mean descriptor

--- FRIDGECOP/objects.py
+++ FRIDGECOP/objects.py
@@ -6,67 +6,21 @@ class Person:
     Parameters:
     name [string]
         The "Firstname Lastname" of the person
-    vocal_desc [Union(tuple,np.array of shape(1,100))]
-        The vocal descriptor (or descriptors) of the person
-    facial_desc [Union(tuple,np.array of shape(1,128))]
-        The facial descriptor (or descriptors) of the person
+    vocal_desc [np.array of shape(1,100)]
+        The mean vocal descriptor of the person
+    facial_desc [np.array of shape(1,128)]
+        The mean facial descriptor of the person
     '''
     
     def __init__(self, name, vocal_desc, facial_desc):
         '''Initializes the Person'''
-        self.name = name
-        
-        if isinstance(vocal_desc,tuple):
-            self.vocal_descriptors = [i for i in vocal_desc]
-            
-        if isinstance(vocal_desc,np.ndarray) or isinstance(vocal_desc,list):
-            self.vocal_descriptors = vocal_desc
-            
-        self.mean_vocal_descriptor = np.mean(self.vocal_descriptors,axis=0)
-        
-        if isinstance(facial_desc,tuple):
-            self.facial_descriptors = [i for i in facial_desc]
-            
-        if isinstance(facial_desc,np.ndarray) or isinstance(facial_desc,list):
-            self.facial_descriptors = facial_desc
-            
-        self.mean_facial_descriptor = np.mean(self.facial_descriptors,axis=0)
+        self.name = name            
+        self.mean_vocal_descriptor = vocal_desc        
+        self.mean_facial_descriptor = facial_desc
 
     def __repr__(self):
         return "FCP: {} ∆ V:{}, F:{}".format(self.name,len(self.vocal_descriptors),len(self.facial_descriptors))
     
-    def add_vocal_descriptor(self,vocal_desc,database):
-        """Adds a vocal descriptor to the person's list
-        
-        Parameters:
-        -----------
-        vocal_desc [np.array] of shape (100,)
-            The vocal to be added
-            
-        database [dict]
-            The database dict of 'name':Person
-        """
-        self.vocal_descriptors.append(vocal_desc)
-        self.mean_vocal_descriptor = np.mean(self.vocal_descriptors,axis = 0)
-        with open('database.p','wb') as f:
-            pickle.dump(database,f)
-    
-    def add_facial_descriptor(self,facial_desc,database):
-        """
-        Adds a facial descriptor to the person's list
-        
-        Parameters:
-        -----------
-        facial_desc [np.array] of shape (128,)
-            The facial descriptor to be added
-        
-        database [dict]
-            The database dict of 'name':Person
-        """
-        self.facial_descriptors.append(facial_desc)
-        self.mean_facial_descriptor = np.mean(self.facial_descriptors,axis = 0)
-        with open('database.p','wb') as f:
-            pickle.dump(database,f)
             
     def vocal_match(self,vocal_desc_to_be_matched,cutoff):
         """
