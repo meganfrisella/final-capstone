0f566b9f834d7b4d9cce2adef44758458b5981b6
Megan Frisella <mmfrisella@wpi.edu>
2019-08-03 14:59:57 -0400

    alexa

--- FRIDGECOP/AlexaFridgeCop.py
+++ FRIDGECOP/AlexaFridgeCop.py
@@ -1,11 +1,7 @@
 from flask import Flask
 from flask_ask import Ask, statement, question
+from import_tests import Model, ClassifyingModel
 from objects import Fridge, check_fridge
-from use_scanned_fridge import Model, ClassifyingModel
-import requests
-import time
-import unidecode
-import json
 import voice_rec
 
 app = Flask(__name__)
@@ -21,21 +17,19 @@ def homepage():
 def start_skill():
     global fridge
     fridge = Fridge()
-    fridge.open_fridge()
-    fridge.add_item(["crab", "gum", "chicken plate", "quiche", "mushroom", "kimchi", "caviar"])
     return question("Fridge Cop on duty. What's the password?")
 
 @ask.intent("WhatsInMyFridgeIntent")
 def open_fridge():
     person = voice_rec.run()
-    print(person)
     items = check_fridge(fridge, person)
-    print(items)
-    return statement(f"Hello, {person}! you have {items} in your fridge.")
+    return statement(items)
 
 @ask.intent("StopIntent")
 def cancel_intent():
     return statement("Alright, adios!")
 
 if __name__ == "__main__":
+    # with open('model7.p', 'rb') as f:
+    #     USF_model = pickle.load(f)
     app.run(debug=True)
